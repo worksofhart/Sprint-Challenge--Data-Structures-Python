bf58a2ede37bbdc909ce694bbff1e6c18f9dbc99
Michael Hart <michael@mhart.dev>
2019-12-20 12:52:55 -0500

    Names stretch using set intersection

--- names/names.py
+++ names/names.py
@@ -28,17 +28,19 @@ duplicates = []
 
 # names_1.sort()
 # names_bst = BinarySearchTree(names_1[len(names_1) // 2])
-names_1.sort()
-
-for name_2 in names_2:
-    if binary_search(names_1, name_2) != -1:
-        duplicates.append(name_2)
-
-end_time = time.time()
-print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish with no restrictions on techniques or data
 # structures?
+
+# names_1.sort()
+# for name_2 in names_2:
+#     if binary_search(names_1, name_2) != -1:
+#         duplicates.append(name_2)
+
+duplicates = (set(names_1) & set(names_2))
+
+end_time = time.time()
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
