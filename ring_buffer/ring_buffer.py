2f98a522e5907edc4e0f61761d5a332ebdc27c2e
Michael Hart <michael@mhart.dev>
2019-12-20 13:45:23 -0500

    Ring buffer MVP

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,28 +4,32 @@ from doubly_linked_list import DoublyLinkedList
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.size = 0
-        self.position = 0
+        self.current = None
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        self.position += 1
-        if self.position > self.capacity:
-            self.storage.remove_from_head()
-            self.storage.add_to_head(item)
-            # self.position = 1
-        else:
+        # If not at capacity yet, add item to the tail and set current position to the head
+        if self.storage.length != self.capacity:
+            self.current = self.storage.head
             self.storage.add_to_tail(item)
+        # Else we're already at capacity
+        else:
+            # So overwrite the current node's value
+            self.current.value = item
+            # And if we're at the tail, we need to wrap back around to the head
+            if self.current is self.storage.tail:
+                self.current = self.storage.head
+            # Otherwise update position to the next node
+            else:
+                self.current = self.current.next
 
     def get(self):
-        # Note:  This is the only [] allowed
         list_buffer_contents = []
         current_node = self.storage.head
-        while current_node is not None:
-            if current_node.value is not None:
-                list_buffer_contents.append(current_node.value)
+
+        while current_node:
+            list_buffer_contents.append(current_node.value)
             current_node = current_node.next
-        print(list_buffer_contents)
 
         return list_buffer_contents
 
@@ -35,14 +39,14 @@ class RingBuffer:
 class ArrayRingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.position = 0
         self.storage = [None] * capacity
+        self.current = 0
 
     def append(self, item):
-        self.storage[self.position] = item
-        self.position += 1
-        if self.position == self.capacity:
-            self.position = 0
+        self.storage[self.current] = item
+        self.current += 1
+        if self.current == self.capacity:
+            self.current = 0
 
     def get(self):
         return [x for x in self.storage if x is not None]
